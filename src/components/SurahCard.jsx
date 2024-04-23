8fa90d7d9020aad650007ef2a452b8046445399f
Huzaifa Khokhar <151424060+KhokharHuzaifa@users.noreply.github.com>
2024-04-23 23:36:48 +0500

    SurahCard.jsx

--- src/components/SurahCard.jsx
+++ src/components/SurahCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useGetWholeQuranQuery } from '../redux/QuranApi'
+import { useGetWholeQuranQuery } from '../redux/QuranApi';
 import { Link } from 'react-router-dom';
 function SurahCard() {
     const { data, error, isLoading } = useGetWholeQuranQuery();
